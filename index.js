770ebfb3d3d190811cc33be459da6878ea8daa62
Kendallst <kendallmebia@gmail.com>
2024-10-13 21:02:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Kenly", "Kenly"];
+const statusMessages = ["vigilando a mi amor kendall", "Kenly"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
